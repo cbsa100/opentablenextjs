643dad2830c5abac8474e4564603007f317da2ba
Sharon Ben Davar <sharon.bendavar@gmail.com>
2023-03-24 21:04:00 +0300

    auth modal

---
+++ app/components/AuthModalInputs.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+interface Props {
+  inputs: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    city: string;
+    password: string;
+  };
+  handleChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isSignIn: boolean;
+}
+const AuthModalInputs = ({ inputs, handleChangeInput, isSignIn }: Props) => {
+  return (
+    <div>
+      {isSignIn ? null : (
+        <div className='my-3 flex text-sm justify-between'>
+          <input
+            type='text'
+            className='border rounded p-2 py-3 w-[49%]'
+            placeholder='First Name'
+            value={inputs.firstName}
+            name='firstName'
+            onChange={handleChangeInput}
+          />
+          <input
+            type='text'
+            className='border rounded p-2 py-3 w-[49%]'
+            placeholder='Last Name'
+            value={inputs.lastName}
+            name='lastName'
+            onChange={handleChangeInput}
+          />
+        </div>
+      )}
+      <div className='my-3 flex-text-sm justify-between'>
+        <input
+          type='text'
+          className='border rounded p-2 py-3 w-full '
+          placeholder='Email'
+          value={inputs.email}
+          name='email'
+          onChange={handleChangeInput}
+        />
+      </div>
+      {isSignIn ? null : (
+        <div className='my-3 flex text-sm justify-between'>
+          <input
+            type='text'
+            className='border rounded p-2 py-3 w-[49%]'
+            placeholder='Phone'
+            value={inputs.phone}
+            name='phone'
+            onChange={handleChangeInput}
+          />
+          <input
+            type='text'
+            className='border rounded p-2 py-3 w-[49%]'
+            placeholder='City'
+            value={inputs.city}
+            name='city'
+            onChange={handleChangeInput}
+          />
+        </div>
+      )}
+      <div className='my-3 flex-text-sm justify-between'>
+        <input
+          type='password'
+          className='border rounded p-2 py-3 w-full '
+          placeholder='Password'
+          value={inputs.password}
+          name='password'
+          onChange={handleChangeInput}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AuthModalInputs;
