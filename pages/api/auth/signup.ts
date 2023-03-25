a0e9e539ada1729a5c5388046c419a81c34651cb
Sharon Ben Davar <sharon.bendavar@gmail.com>
2023-03-25 17:56:37 +0300

    Signin, signup,me endpoints and middleware

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import validator from 'validator';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
+import * as jose from 'jose';
 
 const prisma = new PrismaClient();
 
@@ -60,6 +61,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         email: email,
       },
     });
-    res.status(200).json({ hello: user });
+
+    const alg = 'HS256';
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({ alg })
+      .setExpirationTime('24h')
+      .sign(secret);
+
+    return res.status(200).json({ hello: token });
   }
+  return res.status(404).json('Undifined endpoint');
 }
